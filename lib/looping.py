416e2e2290ff1d12723f7283a89bebef06960ba2
Michael Tanaka <tanakam4214@gmail.com>
2023-07-09 12:35:10 -0700

    Lab Completed

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
+
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(f"{i}")
+        i -= 1
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_numbers = [number * number for number in int_list]
+    return squared_numbers
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i <= 100:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(f"{i}")
+        i += 1
